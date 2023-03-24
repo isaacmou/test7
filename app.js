896391a109345334e30d2858e0b06b630f364922
isaacmou <126214425+isaacmou@users.noreply.github.com>
2023-03-24 21:47:24 +0800

    Add files via upload

---
+++ app.js
@@ -0,0 +1,32 @@
+function showEnglish() {
+    document.getElementById("engVer").style.display = "";
+    document.getElementById("chiVer").style.display = "none";
+    document.getElementById("toptitle").innerHTML = "Fitness Walking Tracks";
+    document.getElementById("engInput").style.display = "";
+    document.getElementById("chiInput").style.display = "none";
+    document.getElementById("engVerwithdistrict").style.display = "none";
+    <!--Show english district button-->
+    
+    
+
+}
+
+function showChinese() {
+    document.getElementById("engVer").style.display = "none";
+    document.getElementById("chiVer").style.display = "";
+    document.getElementById("toptitle").innerHTML = "健步行徑";
+    document.getElementById("engInput").style.display = "";
+    document.getElementById("chiInput").style.display = "none";
+    document.getElementById("engVerwithdistrict").style.display = "none";
+    <!--show chinese button-->
+   
+
+}
+function Area1() {
+    document.getElementById("engVer").style.display = "none";
+    document.getElementById("chiVer").style.display = "none";
+    document.getElementById("toptitle").innerHTML = "Fitness Walking Tracks";
+    document.getElementById("engInput").style.display = "";
+    document.getElementById("chiInput").style.display = "none";
+    document.getElementById("engVerwithdistrict").style.display = "";
+}
